f60c1ee5933aca30d1005abf03de4f9abf12ea39
Ram Iyer <ramky.iyer@gmail.com>
2021-05-02 21:56:41 -0400

    reducing slowdown to 1 million

--- app/controllers/slowdown_controller.rb
+++ app/controllers/slowdown_controller.rb
@@ -2,7 +2,7 @@
 class SlowdownController < ApplicationController
 	def index
 		@result = Benchmark.measure do
-    	1.upto(100_000_000) { }
+    	1.upto(1_000_000) { }
 		end
 		Rails.logger.info("Operation benchmark: #{@result}")
 	end
